4d6a07afec004d017deb55f281aff174c234c672
Diego Borges <drborges.cic@gmail.com>
2015-02-20 09:11:46 -0200

    handles changes to multiple children of a node

--- src/app.js
+++ src/app.js
@@ -3,63 +3,77 @@ import Firebase from 'firebase'
 export class GitflowApp {
 
   constructor() {
-    let context = new Firebase("https://fiery-heat-2090.firebaseio.com/boards/gitflow")
-    this.board = Board.fromContext(context)
+    let context = new Firebase('https://fiery-heat-2090.firebaseio.com/boards')
+
+    let gitflowBoardContext = context.child('gitflow')
+    this.board = new Board().boundTo(gitflowBoardContext)
   }
 }
 
+const EmptyBoardSnapshotVal = {
+  flows: {}
+}
+
+const EmptyFlowSnapshotVal = {
+  issues: {}
+}
+
 class Board {
-  static fromJson(board: Object) {
-    let title = board.title
-    let flows = Object.keys(board.flows).map(f => Flow.fromJson(board.flows[f]))
-    return new Board(title, flows)
+  constructor(snapshotKey, snapshotVal = EmptyBoardSnapshotVal) {
+    this.key = snapshotKey
+    this.title = snapshotVal.title
+    this.flows = [] //Object.keys(snapshotVal.flows).map(flowKey => new Flow(flowKey, snapshotVal.flows[flowKey]))
   }
 
-  static fromContext(context: Firebase) {
-    let board = new Board().bindTo(context)
-    context.on('value', (snapshot) => {
-      let newBoard = Board.fromJson(snapshot.val())
-      board.title = newBoard.title
-      board.flows = newBoard.flows
-      console.log('New Board loaded:', newBoard)
+  boundTo(context) {
+    //this.flows.forEach(flow => flow.boundTo(context.child('flows').child(flow.key)))
+
+    context.child('title').on('value', snapshot => this.title = snapshot.val())
+    context.child('flows').on('child_added', snapshot => {
+      let flowContext = context.child('flows').child(snapshot.key())
+      this.flows.push(new Flow(snapshot.key(), snapshot.val()).boundTo(flowContext))
     })
-    return board
-  }
 
-  constructor(title: String = '', flows: List<Flow> = []) {
-    this.title = title
-    this.flows = flows
-  }
-
-  bindTo(context: Firebase) {
-    this.context = context
     return this
   }
 }
 
 class Flow {
-  static fromJson(flow: Object) {
-    let title = flow.title
-    let issues = Object.keys(flow.issues).map(i => Issue.fromJson(flow.issues[i]))
-    return new Flow(title, issues)
+  constructor(snapshotKey, snapshotVal = EmptyFlowSnapshotVal) {
+    this.key = snapshotKey
+    this.title = snapshotVal.title;
+    this.issues = [] //Object.keys(snapshotVal.issues).map(issueKey => new Issue(issueKey, snapshotVal.issues[issueKey]))
   }
 
-  constructor(title: String, issues: List<Issue> = []) {
-    this.title = title
-    this.issues = issues
+  boundTo(context) {
+    //this.issues.forEach(issue => issue.boundTo(context.child('issues').child(issue.key)))
+
+    context.child('title').on('value', snapshot => this.title = snapshot.val())
+    context.child('issues').on('child_added', snapshot => {
+      let issueContext = context.child('issues').child(snapshot.key())
+      this.issues.push(new Issue(snapshot.key(), snapshot.val()).boundTo(issueContext))
+    })
+
+    return this
   }
 }
 
 class Issue {
-  static fromJson(issue: Object) {
-    return new Issue(issue)
+  constructor(snapshotKey, snapshotVal = {}) {
+    this.key = snapshotKey
+    this.title = snapshotVal.title
+    this.description = snapshotVal.description
+    this.type = snapshotVal.type
+    this.owner = snapshotVal.owner
+    this.createdAt = snapshotVal.createdAt
   }
 
-  constructor(issue: Object) {
-    this.title = issue.title
-    this.description = issue.description
-    this.type = issue.type
-    this.owner = issue.owner
-    this.createdAt = issue.createdAt
+  boundTo(context) {
+    context.on('child_changed', snapshot => {
+      let issueProperty = snapshot.val()
+      this[snapshot.key()] = issueProperty
+    })
+
+    return this
   }
 }
