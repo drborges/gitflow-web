7001fdff8f8ad0f8b85a1c1a60910d5c72d1cb5e
Diego Borges <drborges.cic@gmail.com>
2015-02-21 13:41:21 -0200

    fQuery syntax proposal for 3-way data binding

--- src/app.js
+++ src/app.js
@@ -74,9 +74,3 @@ class Issue {
   }
 }
 
-/* fQuery (a.k.a Firebase Query) syntax proposal:
-
-let reactiveBoard = new Board().boundTo(boardContext).observe('title').observeNewChildOf('flows').observeDeletedChildOf('flows')
-let reactiveFlow  = new Flow().boundTo(flowContext).observe('title').observeNewChildOf('issues').observeDeletedChildOf('issues')
-let reactiveIssue = new Issue().boundTo(issueContext).observeChildren()
-*/
