baeaefbd2e1699dd37c4e3243d3b48a484a7c85a
Diego Borges <drborges.cic@gmail.com>
2015-02-21 13:40:50 -0200

    gs

--- src/app.js
+++ src/app.js
@@ -73,3 +73,10 @@ class Issue {
     return this
   }
 }
+
+/* fQuery (a.k.a Firebase Query) syntax proposal:
+
+let reactiveBoard = new Board().boundTo(boardContext).observe('title').observeNewChildOf('flows').observeDeletedChildOf('flows')
+let reactiveFlow  = new Flow().boundTo(flowContext).observe('title').observeNewChildOf('issues').observeDeletedChildOf('issues')
+let reactiveIssue = new Issue().boundTo(issueContext).observeChildren()
+*/
